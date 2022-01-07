01abfe4dc0e6c4a250fb222a5cdd0c73969b56e3
Pratik Kumar <pratikbablu267@gmail.com>
2022-01-07 23:36:48 +0530

    First Migration | Local NextGen-Snacky DB created | Pratik | V1.0.2

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace NextGen_Snacky.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
