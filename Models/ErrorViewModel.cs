d5224d10ac2b111a38644115cb4b4eab9f9382f0
Pratik Kumar <pratikbablu267@gmail.com>
2022-01-07 23:09:21 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NextGen_Snacky.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
