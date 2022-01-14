be2978d86a36d24d082930dea3511feb4f22340a
Pratik Kumar <pratikbablu267@gmail.com>
2022-01-15 01:56:34 +0530

    Edit.cshtml completed | Pratik | V 2.0.1

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -43,5 +43,21 @@ namespace NextGen_Snacky.Areas.Admin.Controllers
             }
             return View(category);
         }
+
+        //GET - Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            var category = await _adb.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
     }
 }
