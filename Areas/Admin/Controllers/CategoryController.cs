17c8fb41f33f268ecc67f0606fbe67e105375773
Pratik Kumar <pratikbablu267@gmail.com>
2022-01-16 03:31:18 +0530

    Fixed Details.cshtml | Details() implemented | Pratik | V 2.0.4

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -107,5 +107,21 @@ namespace NextGen_Snacky.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
+
+        //GET -Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _adb.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
     }
 }
