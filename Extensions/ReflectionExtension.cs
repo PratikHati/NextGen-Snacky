92a44c4d0331db94e3276577a5b55bb1c27c65f0
Pratik Kumar <pratikbablu267@gmail.com>
2022-01-17 02:30:16 +0530

    Fixed Create.cshtml title in Category |IEnumerableExtension, ReflectionExtension added | Pratik | V 2.0.7

---
+++ Extensions/ReflectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NextGen_Snacky.Extensions
+{
+    public static class ReflectionExtension
+    {
+        public static string GetPropertyValue<T>(this T item, string PropertName)
+        {
+            return item.GetType().GetProperty(PropertName).GetValue(item, null).ToString();
+        }
+    }
+}
