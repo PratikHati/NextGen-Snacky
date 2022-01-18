347a484f12880549cdf4eecea3aaee468da44aff
Pratik Kumar <pratikbablu267@gmail.com>
2022-01-19 00:37:21 +0530

    SubCategory "Create.cshtml" created | POST - Create() implemented | Pratik | V 2.0.8

--- Areas/Admin/Controllers/SubCategoryController.cs
+++ Areas/Admin/Controllers/SubCategoryController.cs
@@ -37,5 +37,36 @@ namespace NextGen_Snacky.Areas.Admin.Controllers
 
             return View(model);
         }
+
+        //POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubCategoryAndCategoryViewModel sub)
+        {
+            if (ModelState.IsValid)
+            {
+                var subcategory = _adb.SubCategory.Include(x => x.Category).Where(x => x.Name == sub.SubCategory.Name && x.Category.Id == sub.SubCategory.CategoryId);
+                if(subcategory.Count() > 0)
+                {
+                    //Error
+                }
+                else
+                {
+                    //valid
+                    _adb.SubCategory.Add(sub.SubCategory);
+                    await _adb.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            SubCategoryAndCategoryViewModel obj = new SubCategoryAndCategoryViewModel
+            {
+                CategoryList = await _adb.Category.ToListAsync(),
+                SubCategory = sub.SubCategory,
+                SubCategoryList = await _adb.SubCategory.OrderBy(x => x.Name).Select(x => x.Name).ToListAsync()
+            };
+
+            return View(obj);
+        }
     }
 }
