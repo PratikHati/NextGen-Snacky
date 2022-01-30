acca8b774261e00619000d11e51f6ffb91fa9b15
Pratik Kumar <pratikbablu267@gmail.com>
2022-01-31 00:38:23 +0530

    Fixed on delete "Cascade" as causing cycle | Changed to "NoAction" | Fixed Migration | :) | Pratik | V 3.0.2

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,6 +16,6 @@ namespace NextGen_Snacky.Data
         }
         public DbSet<Category> Category { get ; set ; }
         public DbSet<SubCategory> SubCategory { get; set; }
-
+        public DbSet<MenuItem> MenuItem { get; set; }
     }
 }
