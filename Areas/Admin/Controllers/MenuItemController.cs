dfccfcffd71a18e5a029db10374f6012616ccfef
Pratik Kumar <pratikbablu267@gmail.com>
2022-02-01 23:43:49 +0530

    MenuItemController Created | Pratik | V3.0.3

---
+++ Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NextGen_Snacky.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NextGen_Snacky.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _adb;
+        private readonly IHostingEnvironment _hosting;
+
+        public MenuItemController(ApplicationDbContext adb, IHostingEnvironment hosting)
+        {
+            _adb = adb;
+            _hosting = hosting;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menu = await _adb.MenuItem.Include(x=>x.Category).Include(x=>x.SubCategory).ToListAsync();
+            return View(menu);
+        }
+    }
+}
