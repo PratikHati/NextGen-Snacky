b35e4acc1662886693616d81007a6dc0eca3db12
Pratik Kumar <pratikbablu267@gmail.com>
2022-02-06 00:21:30 +0530

    BugFree Create.cshtml | Pratik | V3.0.5

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -16,7 +16,13 @@ namespace NextGen_Snacky.Models
         public string Description { get; set; }
 
         public string Spicyness { get; set; }
-        public enum ESpicy { NA = 0, NotSpicy = 1, Spicy = 2, VerySpicy = 3 }
+        public enum ESpicy 
+        { 
+            NA = 0, 
+            NotSpicy = 1, 
+            Spicy = 2, 
+            VerySpicy = 3 
+        }
 
         public string Image { get; set; }
 
