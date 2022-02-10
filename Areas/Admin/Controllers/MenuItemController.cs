268b5aa85db651f26ff7b2967d6556ba0af73db0
Pratik Kumar <pratikbablu267@gmail.com>
2022-02-11 00:17:38 +0530

    GET- Edit() implemented | MenuItemController | Pratik | V3.0.72

--- Areas/Admin/Controllers/MenuItemController.cs
+++ Areas/Admin/Controllers/MenuItemController.cs
@@ -89,5 +89,77 @@ namespace NextGen_Snacky.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+
+        //GET- Edit
+        public async Task<IActionResult> Edit(int ? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            _MenuItemViewModel.MenuItem = await _adb.MenuItem.Include(x => x.Category).Include(x => x.SubCategory).SingleOrDefaultAsync(x=>x.Id==id);
+
+            _MenuItemViewModel.SubCaregory = await _adb.SubCategory.Where(y=>y.CategoryId==_MenuItemViewModel.MenuItem.CategoryId).ToListAsync();
+
+            if (_MenuItemViewModel.MenuItem == null)
+            {
+                return NotFound();
+            }
+            return View(_MenuItemViewModel);
+        }
+
+        //POST- Edit
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPOST(int ? id)      // MenuItemViewModel already binded in constructor so , not need to pass as parameter
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _MenuItemViewModel.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());      //"SubCategoryId" will be collcted from cshtml Form
+
+            if (!ModelState.IsValid)
+            {
+                return View(_MenuItemViewModel);
+            }
+
+            _adb.MenuItem.Add(_MenuItemViewModel.MenuItem);
+            await _adb.SaveChangesAsync();
+
+            //image saving logic
+            string rootpath = _hosting.WebRootPath;
+            var files = HttpContext.Request.Form.Files;             //retreive the file from Create.cshtml uploaded by user
+
+            var menuItemFromDb = await _adb.MenuItem.FindAsync(_MenuItemViewModel.MenuItem.Id);         //retrive the MenuItem ID for the picture
+
+            if (files.Count > 0)
+            {
+                //file already uploaded
+                var upload = Path.Combine(rootpath, "images");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var filestream = new FileStream(Path.Combine(upload + _MenuItemViewModel.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);        //copy file to server(upload)
+                }
+                menuItemFromDb.Image = @"\images\" + _MenuItemViewModel.MenuItem.Id + extension;
+            }
+            else
+            {
+                //files not uploaded, use default 
+                var upload = Path.Combine(rootpath, @"\images\" + SD.DefaultFoodImage);        //use default png file
+
+                System.IO.File.Copy(upload, rootpath + @"\images\" + _MenuItemViewModel.MenuItem.Id + ".png");
+
+                menuItemFromDb.Image = @"\images\" + _MenuItemViewModel.MenuItem.Id + ".png";      //also update menuItemFromDb's Image
+            }
+
+            await _adb.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
