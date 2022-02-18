ce8dc6f7074caee2aee3d3c6ba4ba0a68a654522
Pratik Kumar <pratikbablu267@gmail.com>
2022-02-19 01:39:48 +0530

    Coupon Index() FrontEnd added | Formatted Index.cshtml for all controller ||Pratik | V4.0.2

---
+++ Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NextGen_Snacky.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NextGen_Snacky.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _adb;
+
+        public CouponController(ApplicationDbContext adb)
+        {
+            _adb = adb; 
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _adb.Coupon.ToListAsync());
+        }
+
+    }
+}
